dab996e0caa5df758f6bf57b32807dc860d78202
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-30 19:41:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -21,19 +21,37 @@ var arr2 = [1, 4, 34];
 // document.write(arr);
 //[6,8,19,1,4,34]
 
-var newarr = arr1.concat(arr2);
+// var newarr = arr1.concat(arr2);
 
-console.log('New array:' + newarr);
+// console.log('New array:' + newarr);
 
-for (var i = 0; i < newarr.length - 1; i++) {
-  for (var j = i + 1; j < newarr.length - 1; j++) {
-    if (newarr[i] > newarr[j]) {
-      var temp = newarr[i];
-      newarr[i] = newarr[j];
-      newarr[j] = temp;
-    } else {
-    }
-  }
+// for (var i = 0; i < newarr.length - 1; i++) {
+//   for (var j = i + 1; j < newarr.length - 1; j++) {
+//     if (newarr[i] > newarr[j]) {
+//       var temp = newarr[i];
+//       newarr[i] = newarr[j];
+//       newarr[j] = temp;
+//     } else {
+//     }
+//   }
+// }
+
+// console.log('Sorted array:' + newarr);
+
+// var str1 = 'apple, banana, orange';
+
+// console.log(str1.match('an'));
+
+const numbers = [45, 4, 9, 16, 25];
+
+function myFunction(value, index, array) {
+  // console.log(value);
+  var a = 1;
+  return a;
 }
 
-console.log('Sorted array:' + newarr);
+let txt = numbers.forEach(myFunction);
+
+// document.getElementById("demo").innerHTML = txt;
+// console.log(txt);
+console.log(numbers.forEach(myFunction));
